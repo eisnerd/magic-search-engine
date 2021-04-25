cc6f2671686481da0fb6a8e46b715ae257a8eeae
David Eisner <david.eisner@oriel.oxon.org>
2021-04-25 12:53:41 +0100

    Lower memory price import
    with the all_cards json object being pre-processed with jq:
    
    wget -qO- $(wget -qO- https://api.scryfall.com/bulk-data|jq -r '.data[]|select(.type == "all_cards")|.download_uri') | jq -c '.[] | {set, collector_number, prices}' > all-cards-prices.json

--- indexer/lib/patches/patch_scryfall_prices.rb
+++ indexer/lib/patches/patch_scryfall_prices.rb
@@ -1,8 +1,13 @@
 # Merge with price data from scryfall bulk export
+
+require 'fast_jsonparser'
 class PatchScryfallPrices < Patch
   @@scryprice = nil
   def call
-    @@scryprice ||= JSON.parse(File.read("all-cards.json")).map {|card| ["#{card["set"]}/#{card["collector_number"]}", card["prices"]] }.to_h
+    unless @@scryprice
+      @@scryprice = {}
+      FastJsonparser.load_many("all-cards-prices.json") { |card| prices = @@scryprice["#{card[:set]}/#{card[:collector_number]}"] ||= {}; prices.merge!(card[:prices].compact) }
+    end
     each_printing do |card|
       next unless card["set"] and card["number"]
       prices = @@scryprice["#{card["set"]["code"]}/#{card["number"]}"] rescue nil
